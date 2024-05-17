c8c2100602e9c1c21b9a8bea8cce7c1f481f9ebe
francomac <franco.mcarballo@outlook.com>
2024-05-16 23:18:10 -0600

    dashboard added

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import "./Dashboard.scss";
+
+function Dashboard() {
+  return (
+    <>
+      <div className="dashboard-grid-container">
+        <div className="grid-item header">1</div>
+        <div className="grid-item menu">2</div>
+        <div className="grid-item body">3</div>
+        <div className="grid-item footer">4</div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
